62d8a57e8559684ac70bd5a578236fc13e00d425
tanbro <tanbro@163.com>
2023-02-17 18:27:30 +0800

    一些优化

--- try_chatgpt.py
+++ try_chatgpt.py
@@ -1,4 +1,5 @@
-from os import getenv
+from os import environ, getenv
+from textwrap import dedent
 
 import openai
 from dotenv import load_dotenv
@@ -6,19 +7,27 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    openai.api_key = getenv("OPENAI_API_KEY")
+    openai.api_key = environ["OPENAI_API_KEY"]
     #
     kdargs = dict(
-        model=getenv("OPENAI_COMPLETION_MODEL") or "text-davinci-003",
-        max_tokens=int(getenv("OPENAI_COMPLETION_MAX_TOKENS", 0)) or 1024,
+        model=getenv("OPENAI_COMPLETION_MODEL") or "text-ada-001",
         stream=True,
     )
+    max_tokens = getenv("OPENAI_COMPLETION_MAX_TOKENS")
+    if max_tokens:
+        kdargs.update(max_tokens=int(max_tokens))  # type: ignore
     #
     try:
         while True:
-            prompt_string = input("🧑💬 : ").strip()
-            if not prompt_string:
+            input_string = input("🧑💬 : ").strip()
+            if not input_string:
                 continue
+
+            prompt_string = dedent(f"""
+            Q: {input_string}
+            A:
+            """).strip()
+
             print()
             print("🤖💬 : ", end="", flush=True)
 
@@ -27,10 +36,14 @@ def main():
                 **kdargs
             )
 
-            for i, x in enumerate(stream):
+            is_empty = True
+            for x in stream:
                 s = x["choices"][0]["text"]  # type: ignore
-                s = s if i else s.lstrip()
-                print(s, end="", flush=True)
+                if is_empty and s.strip():
+                    is_empty = False
+                    s = s.lstrip()
+                if not is_empty:
+                    print(s, end="", flush=True)
 
             print()
             print('━'*30)
